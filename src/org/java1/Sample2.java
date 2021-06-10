b2d2d0778caf8a0e54b02fcb38bdd8b483e8a3ec
satzmech90 <satzmech90@gmail,com>
2021-06-10 10:42:04 +0530

    new class

---
+++ src/org/java1/Sample2.java
@@ -0,0 +1,16 @@
+package org.java1;
+
+public class Sample2 {
+	private void name(String name) {
+		System.out.println("Name is "+name);
+	}
+	private void DOB(Long Adhar) {
+		System.out.println("Adhar number is "+ Adhar);
+	}
+	
+	
+	
+	
+	
+
+}
